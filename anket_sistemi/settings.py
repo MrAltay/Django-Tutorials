0f1b1f3ed569fef1d34073db6306ff6f4aa9d12a
altay <altay3954@gmail.com>
2026-03-10 00:25:01 +0300

    Rename polls app to anketler

--- anket_sistemi/settings.py
+++ anket_sistemi/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'anketler.apps.AnketlerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- anketler/views.py
+++ anketler/views.py
@@ -9,7 +9,7 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-    template_name = "polls/index.html"
+    template_name = "anketler/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
@@ -22,7 +22,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = "polls/detail.html"
+    template_name = "anketler/detail.html"
     
     def get_queryset(self):
         """
@@ -33,7 +33,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = "polls/results.html"
+    template_name = "anketler/results.html"
 
 
 def vote(request, question_id):
@@ -44,7 +44,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(
             request,
-            "polls/detail.html",
+            "anketler/detail.html",
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
@@ -56,4 +56,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("anketler:results", args=(question.id,)))
